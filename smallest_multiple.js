a546ab56e7fe9f73b38b13eb3a826efa57da2ca3
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-09-20 10:54:01 -1000

    teacher code uploaded. personal code still needs to be worked out

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -1,3 +1,4 @@
+//jshint esversion: 6
 /**
  * Build a function that finds the smallest positive number that is evenly
  * divisible by all of the numbers starting from 1 up to the value passed into your function.
@@ -6,9 +7,69 @@
  * @return { Number }         Lowest Positive Number that is evenly divisible by all numbers
  *                            between 1 and `ceiling`
  */
-module.exports = function( ceiling ) {
-  // do work here
 
+//***Krissy's notes:
+//the smallest positive number that is evenly divided by a given set of numbers is called the LEAST COMMON MULTIPLE (LCM).
+//-----> We find LCM by using Euclid's algorithm
 
-  return 0;
-};
+// module.exports = function( ceiling ) {
+//   // do work here
+//   let smallestMult = 2520;
+//   let dividesEven = false;
+//   let divisible = true;
+
+//   while( !dividesEven ) {
+//     smallestMult += 2520;
+//     divisible = true;
+//     for (var i = 11; i < ceiling +1; i++) {
+//       if (smallestMult % i !== 0) {
+//         divisible = false;
+//       }else{
+//       dividesEven = true;
+//     }
+//   }
+//   return smallestMult;
+//   }
+// }
+// <<<<<...still working the logic/code out...>>>
+
+//////////////////TEACHER ANSWER///////////////////
+
+function smallestMultiple(num) {
+  if (num === 1) { return 1; }
+
+  // generate factors from 1 to num
+  let factors = [];
+  for (let i = 2; i <= num; i++) {
+    factors.push(i);
+  }
+
+  // test all numbers between 1 -> Infinity
+  // check if all the factors % === 0
+  let result;
+  while (!result) {
+    let allFactorsPass = true;
+
+    // check all factors
+    for(let i = 0; i < factors.length; i++) {
+      if (num % factors[i] !== 0) {
+        allFactorsPass = false;
+        break;
+      }
+    }
+
+    // if all factors pass set result to num
+    if (allFactorsPass) {
+      result = num;
+    }
+    else {
+      num++;
+    }
+  }
+
+  return result;
+}
+
+console.log(smallestMultiple(20));
+
+module.exports = smallestMultiple;
